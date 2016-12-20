acf0b900bb3592ab3edd3b087accbd978c217353
Jason Gurwin <jgurwin@gmail.com>
2016-12-20 22:52:10 +0000

    added redirect after signup and basic profile page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,7 +13,7 @@ class ProfilesController < ApplicationController
     
     if @profile.save
       flash[:success] = "Profile Updated!"
-      redirect_to root_[path]
+      redirect_to user_path( params[:user_id] )
     else
       render action: :new
     end 
